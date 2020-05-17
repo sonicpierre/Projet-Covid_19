0498b795a1e39da471940d3a264404f619ec7af0
Smaxy63 <jbuissonch@gmail.com>
2020-05-17 23:15:31 +0200

    Historique

--- src/modele/territoire/France.java
+++ src/modele/territoire/France.java
@@ -56,21 +56,18 @@ public class France {
 			BufferedReader br = new BufferedReader(fr);
 			try {
 				String ligne = br.readLine();
-				while (ligne != null) {
+				while ((ligne = br.readLine()) != null) {
 					String champs[] = ligne.split(";");
 					try {
 						Class.forName("com.mysql.cj.jdbc.Driver");
 						try (Connection conn = DriverManager.getConnection(url, user, passwd)) {
 							Statement stat = conn.createStatement();
 							stat.executeUpdate("CREATE TABLE IF NOT EXISTS `Historique`("
-									+ "`departement` VARCHAR(3),"
-									+ "`date` VARCHAR(10),"
-									+ "`hospitalises` INT,"
-									+ "`reanimation` INT,"
-									+ "`gueris` INT,"
-									+ "`morts` INT,"
-									+ "CONSTRAINT pk_Historique PRIMARY KEY(`departement`,`date`),"
-									+ "FOREIGN KEY fk_departement(departement) REFERENCES Department(code));");
+									+ "`departement` VARCHAR(3) COLLATE utf8mb4_unicode_ci NOT NULL,"
+									+ "`date` VARCHAR(10)," + "`hospitalises` INT," + "`reanimation` INT,"
+									+ "`gueris` INT," + "`morts` INT," + "PRIMARY KEY(`departement`,`date`),"
+									+ "KEY `departmentCode` (`departement`),"
+									+ "CONSTRAINT `departmentCode` FOREIGN KEY (`departement`) REFERENCES `departments` (`code`) ON DELETE CASCADE);");
 							stat.executeUpdate("INSERT IGNORE Historique SET `departement`=" + champs[0] + ",`date`='"
 									+ champs[2] + "',`hospitalises`=" + Integer.parseInt(champs[3]) + ",`reanimation`="
 									+ Integer.parseInt(champs[4]) + ",`gueris`=" + Integer.parseInt(champs[5])
