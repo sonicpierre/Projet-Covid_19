960779048ef95bf35339732b8bec40ec2442124b
chtryroxa <chtryroxa@eisti.eu>
2020-05-19 15:58:01 +0200

    commit Coordonnees

--- src/Test/TestModel.java
+++ src/Test/TestModel.java
@@ -1,12 +1,20 @@
 package Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jxmapviewer.viewer.GeoPosition;
+
 import modele.territoire.France;
+import modele.trajectoire.Positionnement;
 
 public class TestModel {
 
 	public static void main(String[] args) {
-		France.getInstance().ajouterHistoriqueBDD();
-
+//		France.getInstance().ajouterHistoriqueBDD();
+//		Positionnement pos = new Positionnement();
+//		List<GeoPosition> listeCoor = pos.positionner2Villes("Basta", "Rodez");
+//		System.out.println(listeCoor);
 	}
 
 }
