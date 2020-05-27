2f226fbfb927dced3f49566b5f84a206b213e142
RoxaneChatry <65353688+RoxaneChatry@users.noreply.github.com>
2020-05-27 11:33:33 +0200

    commentaires initlialisationBDD

--- src/modele/BDD/InitialisationBDD.java
+++ src/modele/BDD/InitialisationBDD.java
@@ -5,19 +5,43 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+
 public class InitialisationBDD {
-	private static final String url2 = "jdbc:mysql://localhost/?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	/**
+	 * url d'accès à la bdd sans table 
+	 */
+	private static final String url = "jdbc:mysql://localhost/?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	/**
+	 * nom d'utilisateur
+	 */
 	public static String user;
+	/**
+	 * mot de passe utilisateur
+	 */
 	public static String passwd;
 	
+	/** définit le mot de passe utilisateur
+	 * 
+	 * @param mdp la mot de passe à utiliser
+	 */
 	private void setPasswd(String mdp) {
 		InitialisationBDD.passwd = mdp;
 	}
+	/** définit le nom d'utilisateur
+	 * 
+	 * @param login le nom à utiliser
+	 */
 	private void setUser(String login) {
 		InitialisationBDD.user = login;
 	}
 	
-	// constructeur 
+	/**
+	 * initialise la bdd avec les données utilisateur fournies, dans le cas où il faut créer un user (mot de passe root donné)
+	 * @param login le nom d'utilisateur
+	 * @param mdp le mot de passe utilisateur
+	 * @param mdpRoot el mot de passe root
+	 * @return un booléen indiquant que tout a fonctionné
+	 */
 	public boolean initialiserBDD(String login, String mdp, String mdpRoot) { 
 		// vérification que l'user n'existe pas 
 		boolean existe = this.existeUser(login, mdp);
@@ -34,6 +58,12 @@ public class InitialisationBDD {
 		return(false);
 	}
 
+	/** 
+	 * Initialise la bdd avec les données utilisateurs fournies si l'utilisateur existe déjà (pas de mot de passe root fourni)
+	 * @param login le nom utilisateur
+	 * @param mdp le mot de passe utilisateur
+	 * @return un booléen indiquant que tout a fonctionné
+	 */
 	public boolean initialiserBDD(String login, String mdp) { 
 		if (this.existeUser(login,mdp)) {
 			this.setUser(login);
@@ -43,11 +73,16 @@ public class InitialisationBDD {
 		return(false);
 	}
 	
-	// vérifie que l'user existe + crée la database France si elle n'existe pas
+	/**
+	 * vérifie que l'user existe + crée la database France si elle n'existe pas
+	 * @param login le nom d'utilisateur
+	 * @param mdp le mot de passe utilisateur
+	 * @return un booléen indiquant si le couple login/mdp est valable (connection possible à la bdd)
+	 */	
 	public boolean existeUser(String login, String mdp) {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			try (Connection conn = DriverManager.getConnection(url2, login, mdp)) {
+			try (Connection conn = DriverManager.getConnection(url, login, mdp)) {
 				Statement stat = conn.createStatement();
 				stat.executeUpdate("CREATE DATABASE IF NOT EXISTS France;");
 				stat.close();
@@ -61,13 +96,20 @@ public class InitialisationBDD {
 		}
 		return(true);
 	}
-	
+
+	/**
+	 * crée un user 
+	 * @param login le nom d'utilisateur
+	 * @param mdp le mot de passe utilisateur
+	 * @param mdpRoot le mot de passe root
+	 * @return un booléen indiquant si tout a fonctionné
+	 */
 	public boolean creerUser(String login, String mdp, String mdpRoot) {
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
-		try(Connection conn = DriverManager.getConnection(url2, "root", mdpRoot)){
+		try(Connection conn = DriverManager.getConnection(url, "root", mdpRoot)){
 			System.out.println("Connexion effective !");
 		
 			Statement stat = conn.createStatement();
